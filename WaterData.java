885cfd6d89d2f7b26d6480c3af11f63d25dbb048
IanChak <60276491+IanTimchak@users.noreply.github.com>
2020-01-28 10:29:46 -0500

    Update WaterData.java

--- WaterData.java
+++ WaterData.java
@@ -1,18 +1,14 @@
 import java.util.*;
 
-public class WaterData
-{
-    /*private double cond;
-    private double ph;
-    private double ppm;
-    */
-    
+public class WaterData{
     private ArrayList <String> indices = new ArrayList <String> (0); //marks the indices of each sample tested
     private ArrayList ph = new ArrayList(0); //recording pH levels of water sample
     private ArrayList ppm = new ArrayList(0); //records ppm of water sample
     
-    private int mInd = -1;
-    private int tang = 0;
+    private int mInd = -1;//used for validating index calls
+    
+    public WaterData(){
+    }//constructor @WaterData
     
     public void add(String sName, double p, double parts){//method used for adding new samples to the arrays
         mInd = indices.size();
@@ -20,9 +16,6 @@ public class WaterData
         ph.add(p);
         ppm.add(parts);
     }//method @add
-    
-    public WaterData(){
-    }//constructor @WaterData
     
     public String sample(){//select a specific sample
       Scanner sc = new Scanner(System.in);
@@ -36,13 +29,19 @@ public class WaterData
         }
     }//method @sample
     
+    public String remove(int ind){//remove an index from the sample array
+      int size = indices.size() - 1;
+      System.out.println(indices.get(ind) + " Has been removed.");
+      
+      indices.remove(ind);
+      
+    }
     
-    
-    public String toString(){
+    public String toString(){//returns the list of samples when the class is called
       String a = "";
       for(String b: indices){
         a += b + "; ";
       }
       return a;
-    }
+    }//method @toString
 }
