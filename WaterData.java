6c74c7362d53d32773338b336333d0a343471665
IanChak <60276491+IanTimchak@users.noreply.github.com>
2020-02-10 11:53:48 -0500

    Update WaterData.java

--- WaterData.java
+++ WaterData.java
@@ -23,56 +23,38 @@ import java.util.*;
   *Be able to search for a specific WaterSample object based on its unique ID
   *Be able to call certain methods of a WaterSample object
  */
+import java.util.*;
 
-public class WaterData{
-    private ArrayList <String> indices = new ArrayList <String> (0); //marks the indices of each sample tested
-    private ArrayList ph = new ArrayList(0); //recording pH levels of water sample
-    private ArrayList ppm = new ArrayList(0); //records ppm of water sample
+public class WaterData
+{
+  private ArrayList <WaterSample> samples = new ArrayList<WaterSample>();
+  
+/**
+ * Default Constructor
+ */ 
+  public WaterData(){}
+  
+/**
+ * Smallest method for add()
+ * @param newWS A new WaterSample class being added to the WaterData list for analysis
+ */
+  public void add(WaterSample newWS){
+    samples.add(newWS);
+  }//add(WS)
+  
+/**
+   * toString method
+   */
+  public String toString(){
+    String idList = "";
     
-    private int mInd = -1;//used for validating index calls
-    
-    public WaterData(){
-    }//constructor @WaterData
-    
-    public void add(String sName, double p, double parts){//method used for adding new samples to the arrays
-        mInd = indices.size();
-        indices.add(indices.size() + ". " + sName); //creates a value in indices to connect the information of each arraylist
-        ph.add(p);
-        ppm.add(parts);
-    }//method @add
-    
-    public String sample(){//select a specific sample
-      Scanner sc = new Scanner(System.in);
-      
-        System.out.println("Enter an index to search:");
-        int in = sc.nextInt();
-        if(in > mInd || in < 0)
-            return "ERROR: No sample data found";
-        else{
-            return indices.get(in) + "\n\t" + "pH level: " + ph.get(in) + "\n\tppm level: " + ppm.get(in);
-        }
-    }//method @sample
-    
-    public void remove(int ind){//remove an index from the sample array NOTE: STILL NEEDS PH AND PPM
-        int size = indices.size() - 1;
-        System.out.println(indices.get(ind) + " Has been removed.");
-      
-        indices.remove(ind);
-      
-        for(int i = ind; i < indices.size() - 1; i++){
-            String str = indices.get(i);
-          
-            str += i + str.substring(1, indices.get(i).length());
-          
-            indices.set(i, str);
-        }
-    }
-    
-    public String toString(){//returns the list of samples when the class is called
-      String a = "";
-      for(String b: indices){
-        a += b + "; ";
-      }
-      return a;
-    }//method @toString
+    for(WaterSample a: samples){
+      if(samples.indexOf(a) == samples.size())
+        idList += a.getID();
+      else
+        idList += a.getID() + ", ";
+    }//for
+        
+    return "Samples: " + idList;
+  }//toString
 }
