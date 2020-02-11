61da9d59b04d2bd6d3c25000b6ede765325f08ba
IanChak <60276491+IanTimchak@users.noreply.github.com>
2020-02-11 10:29:42 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,9 +7,9 @@ public class Main
     
     
     WaterSample test = new WaterSample();
-    masterClass.add(test);
     WaterSample test2 = new WaterSample("test", 7.2, 120, "kinda orangee", "Sulfur mine");
     masterClass.add(test2);
+    masterClass.add(test);
     
     System.out.println(masterClass);
   }//@main
